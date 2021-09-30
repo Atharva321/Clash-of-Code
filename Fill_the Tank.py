44cd7e1708ef255a7e517cac440f16d694bf9170
Atharva321 <44301928+Atharva321@users.noreply.github.com>
2021-10-01 01:06:29 +0530

    Create Fill_the Tank.py

---
+++ Fill_the Tank.py
@@ -0,0 +1,20 @@
+"""
+You need to calculate if it is possible to fuel up a spaceship within the number of seconds allowed(t), given the capacity of the tank (c) in litres, and the number of litres of fuel that can be pumped into its tank per second (r).
+
+The tank starts empty.
+The maximum amount of litres per second (r) is constantly pumped into the tank until it is full.
+Input
+Line 1: Three space seperated integers t, c, and r.
+Output
+Line 1: An string "yes" or "no". "yes" if it is possible to fill up the spaceship in the time allowed, "no" if it is not possible.
+Constraints
+0 < t ≤ 10,000
+0 < c ≤ 10,000
+0 < r ≤ 1000
+
+
+"""
+
+
+t, c, r = [int(i) for i in input().split()]
+print("yes" if c<=r*t else "no")
