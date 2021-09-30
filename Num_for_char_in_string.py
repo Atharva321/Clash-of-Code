ebb570740e31df4be76acdcc8a961cf30ee843c5
Atharva321 <44301928+Atharva321@users.noreply.github.com>
2021-10-01 01:54:14 +0530

    Create Num_for_char_in_string.py

---
+++ Num_for_char_in_string.py
@@ -0,0 +1,19 @@
+import sys
+import math
+
+
+s = input().split(" ")
+e=""
+for i in s:
+    a=list(i)
+    b=[x.upper() for x in a if str(x).isalpha()]
+    b=list(set(b))
+    b=sorted(b,key=ord)
+    
+    for w in i:
+        if w.upper() in b:
+            e+=str(b.index(w.upper())+1)
+        else:
+            e+=w
+    e+=" "
+print(e[:-1])
