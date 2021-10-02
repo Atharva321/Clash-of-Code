d1c3113aae9ea069f574cd4ceb66e81aac2dac03
Atharva321 <44301928+Atharva321@users.noreply.github.com>
2021-10-02 08:47:14 +0530

    Create Linear_Function.py

---
+++ Linear_Function.py
@@ -0,0 +1,15 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+a, b = [int(i) for i in input().split()]
+n = int(input())
+for i in range(n):
+    x = int(input())
+    print(a*x+b)
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
