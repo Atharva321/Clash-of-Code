9c4d2f8524441b8d3821158594f4a710873c88c5
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2022-02-05 23:09:03 +0530

    Update Hex_RGB_to_Decimal.py

--- Hex_RGB_to_Decimal.py
+++ Hex_RGB_to_Decimal.py
@@ -6,10 +6,10 @@ while(j<6):
  j+=2
 
 """
-  
+converting the RGB tod decimal.
+for single sentence we cna write the body statement 
+after the loop syntax
 Shortest Solution
-
 c=input()
 for i in range(1,7,2):print(int(c[i:i+2],16))
-
 """
